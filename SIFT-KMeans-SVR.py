408c9c774ff0403571551406d91c8fdec24a90ed
EvelynAbilova <79151653+EvelynAbilova@users.noreply.github.com>
2023-04-20 21:43:45 +0300

    Update SIFT-KMeans-SVR.py

--- SIFT-KMeans-SVR.py
+++ SIFT-KMeans-SVR.py
@@ -67,14 +67,44 @@ print('Mean absolute error: {:.2f}'.format(mae))
 # Save the trained model
 dump(svr, 'svr_model.joblib')
 
-# Display an image from the test set with the predicted and actual counts
-img_path = os.path.join(MALL_DIR, 'frames', 'frames', 'seq_000001.jpg')
-img = cv2.imread(img_path)
-actual_count = labels_df[labels_df['id'] == 1]['count'].values
-predicted_count = svr.predict(X_test[0].reshape(1,-1))[0]
-ax = plt.subplot(1, 1, 1)
-ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-ax.axis('off')
-ax.text(10, 30, 'Actual count: {}'.format(actual_count[0]), color='white', fontsize=10, bbox=dict(facecolor='red', alpha=0.5))
-ax.text(10, 60, 'Predicted count: {:.2f}'.format(predicted_count), color='white', fontsize=10, bbox=dict(facecolor='blue', alpha=0.5))
+# Save the predicted and actual counts for each test sample
+results = []
+for i in range(len(y_test)):
+    img_path = os.path.join(MALL_DIR, 'frames', 'frames', 'seq_{:06d}.jpg'.format(i+1))
+    actual_count = int(y_test[i])
+    predicted_count = int(round(y_pred[i]))
+    results.append((img_path, predicted_count, actual_count))
+
+# Sort the results by the difference between actual count and predicted count
+results.sort(key=lambda x: abs(x[2] - x[1]))
+
+# Select the best and worst results based on prediction accuracy
+best_result = results[0]
+worst_result = results[-1]
+
+# Calculate the prediction accuracy for the best and worst results
+best_accuracy = (1 - abs(best_result[1] - best_result[2]) / best_result[1]) * 100
+worst_accuracy = (1 - abs(worst_result[2] - worst_result[1]) / worst_result[2]) * 100
+
+# Display the best and worst results
+plt.figure(figsize=(10, 5))
+
+# Display the best result
+plt.subplot(1, 2, 1)
+img = cv2.imread(best_result[0], cv2.IMREAD_COLOR)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+plt.imshow(img)
+plt.axis('off')
+plt.title('Best score\nActual count: {}\nPredicted count: {}\nAccuracy: {:.2f}%'.format(
+    best_result[2], best_result[1], best_accuracy))
+
+# Display the worst result
+plt.subplot(1, 2, 2)
+img = cv2.imread(worst_result[0], cv2.IMREAD_COLOR)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+plt.imshow(img)
+plt.axis('off')
+plt.title('Worst score\nActual count: {}\nPredicted count: {}\nAccuracy: {:.2f}%'.format(
+    worst_result[2], worst_result[1], worst_accuracy))
+
 plt.show()
